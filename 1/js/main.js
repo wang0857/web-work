d5ca95810be8df67f95dd90114d48e32bf722790
Ruo-Fang Wang <wang0857@algonquinlive.com>
2022-01-13 15:05:37 -0500

    Create work 1 and 2 folders

---
+++ 1/js/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+    // ----------------Mobile Menu Display----------------
+    $(".nav-menu-bars").click(function(){
+        $(".navlist").slideToggle();
+    });
+
+    $(".navlist").click(function(){
+        $(".navlist").slideToggle();
+    });
+
+});
+
+
+
+
